826014af9a7028b0652e03f8330c229e88f33592
Febriana Ida Nugraheni <115141871+FEBRIANA1602@users.noreply.github.com>
2024-05-20 11:33:08 +0700

    Create data.dart

---
+++ KlinikApp/lib/model/data.dart
@@ -0,0 +1,75 @@
+List<Map> pegawai = [
+  //penggunaan List<Map> untuk menyimpan data pegawai.
+  //Setiap elemen dalam List<Map> adalah sebuah Map yang mewakili satu entitas pegawai dengan atribut-atribut yang sesuai.//
+  {
+    'id': 1,
+    'nip': '12220227',
+    'nama': 'Febriana Ida Nugraheni',
+    'tgllahir': '16 Februari 2003',
+    'nmrtlp': '085640409250',
+    'email': 'febriana88@gmail.com',
+    'password': '123',
+  },
+  {
+    'id': 2,
+    'nip': '12220446',
+    'nama': 'Selviana Dwi Sari Arsanti',
+    'tgllahir': '17 Mei 2004',
+    'nmrtlp': '08999888777',
+    'email': 'Selvianadwi@gmail.com',
+    'password': '456',
+  },
+  {
+    'id': 3,
+    'nip': '12220975',
+    'nama': 'Dhini Fia Julianti',
+    'tgllahir': '21 Juli 2004',
+    'nmrtlp': '085554444333',
+    'email': 'Dhinifia@gmail.com',
+    'password': '789',
+  },
+  {
+    'id': 4,
+    'nip': '12220753',
+    'nama': 'Grace Selia Omega',
+    'tgllahir': '9 Juni 2004',
+    'nmrtlp': '083116666999',
+    'email': 'graceselia@gmail.com',
+    'password': '101',
+  },
+];
+
+List<Map> pasien = [
+  {
+    'id': 1,
+    'noRm': '104',
+    'nama': 'Tesalonika Widyawara',
+    'tgllahir': '4 Oktober 2002',
+    'nmrtlp': '081122223333',
+    'alamat': 'Klaten',
+  },
+  {
+    'id': 2,
+    'noRm': '117',
+    'nama': 'Trifosa meira',
+    'tgllahir': '13 Juni 2003',
+    'nmrtlp': '084455556666',
+    'alamat': 'Boyolali',
+  },
+  {
+    'id': 3,
+    'noRm': '121',
+    'nama': 'Kirana sabrina',
+    'tgllahir': '2 Mei 2003',
+    'nmrtlp': '087788889999',
+    'alamat': 'Sukoharjo',
+  },
+  {
+    'id': 4,
+    'noRm': '109',
+    'nama': 'Ansela Putri',
+    'tgllahir': '16 September 2005',
+    'nmrtlp': '085566778899',
+    'alamat': 'Karanganyar',
+  },
+];
