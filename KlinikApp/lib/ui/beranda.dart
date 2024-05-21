d06cfb92d8fe3b873439f7438f05d893ef435e8b
Febriana Ida Nugraheni <115141871+FEBRIANA1602@users.noreply.github.com>
2024-05-21 13:23:07 +0700

    Update beranda.dart

--- KlinikApp/lib/ui/beranda.dart
+++ KlinikApp/lib/ui/beranda.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
-
+//mengimpor paket Flutter 
 class Beranda extends StatelessWidget {
+  //mendefinisikan sebuah kelas bernama Beranda
   const Beranda({super.key});
-
+  //mendefinisikan sebuah kelas bernama Beranda
   @override
   Widget build(BuildContext context) {
+    //mendefinisikan sebuah kelas bernama Beranda
     return Scaffold(
+      //mengembalikan sebuah widget Scaffold
       appBar: AppBar(
+        //mengembalikan sebuah widget Scaffold
         title: const Text("Beranda"),
+        //"Beranda" sebagai judul AppBar
       ),
       body: const Center(
+        //mendefinisikan konten utama dari halaman
         child: Text("Selamat Datang"),
-      ),
+      ),//menambahkan sebuah teks "Selamat Datang" sebagai anak dari widget Center.
     );
   }
 }
